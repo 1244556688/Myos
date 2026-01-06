74cfa9a1cdb7a4b5a3594f0a3298a3c97aebc353
1244556688 <o396g4h2@gmail.com>
2026-01-06 19:28:11 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,59 +7,79 @@ BUILD_DIR=$WORKDIR/build
 ISO_DIR=$WORKDIR/iso
 
 # 清理舊檔
-rm -rf $BUILD_DIR $ISO_DIR
-mkdir -p $BUILD_DIR $ISO_DIR
+rm -rf "$BUILD_DIR" "$ISO_DIR"
+mkdir -p "$BUILD_DIR" "$ISO_DIR"
 
-# 1. debootstrap 安裝基本系統
-debootstrap --variant=minbase bookworm $BUILD_DIR http://deb.debian.org/debian/
+# 1. debootstrap
+debootstrap --arch=amd64 bookworm "$BUILD_DIR" http://deb.debian.org/debian/
 
-# 2. chroot 裡安裝桌面環境
-cat > $BUILD_DIR/root/setup.sh <<'EOF'
+# 掛載必要系統
+mount --bind /dev "$BUILD_DIR/dev"
+mount -t proc /proc "$BUILD_DIR/proc"
+mount -t sysfs /sys "$BUILD_DIR/sys"
+
+# 2. chroot 設定系統 + 安裝 kernel
+cat > "$BUILD_DIR/root/setup.sh" <<'EOF'
 #!/bin/bash
 set -e
+
 apt-get update
-apt-get install -y lxde-core lightdm sudo locales
+
+apt-get install -y \
+  linux-image-amd64 \
+  live-boot \
+  systemd-sysv \
+  lxde-core \
+  lightdm \
+  sudo \
+  locales
+
 locale-gen en_US.UTF-8
 echo "myos" > /etc/hostname
 
-# 建一個普通使用者 myuser，密碼是 password
 useradd -m -s /bin/bash myuser
 echo 'myuser:password' | chpasswd
 usermod -aG sudo myuser
 
-# 設定 LightDM 自動登入
 cat > /etc/lightdm/lightdm.conf <<EOL
-[SeatDefaults]
+[Seat:*]
 autologin-user=myuser
 EOL
 
-rm -rf /root/setup.sh
+update-initramfs -c -k all
+
+rm -f /root/setup.sh
 EOF
 
-chmod +x $BUILD_DIR/root/setup.sh
-chroot $BUILD_DIR /root/setup.sh
+chmod +x "$BUILD_DIR/root/setup.sh"
+chroot "$BUILD_DIR" /root/setup.sh
 
-# 3. 製作 squashfs
-mkdir -p $ISO_DIR/live
-mksquashfs $BUILD_DIR $ISO_DIR/live/filesystem.squashfs -e boot
+# 卸載
+umount "$BUILD_DIR/dev"
+umount "$BUILD_DIR/proc"
+umount "$BUILD_DIR/sys"
 
-# 4. 複製 kernel 與 initrd
-cp $BUILD_DIR/boot/vmlinuz* $ISO_DIR/live/vmlinuz
-cp $BUILD_DIR/boot/initrd.img* $ISO_DIR/live/initrd.img
+# 3. squashfs
+mkdir -p "$ISO_DIR/live"
+mksquashfs "$BUILD_DIR" "$ISO_DIR/live/filesystem.squashfs" -e boot
 
-# 5. 製作 grub 配置檔
-mkdir -p $ISO_DIR/boot/grub
-cat > $ISO_DIR/boot/grub/grub.cfg <<EOF
+# 4. 複製 kernel / initrd（現在一定存在）
+cp "$BUILD_DIR"/boot/vmlinuz-* "$ISO_DIR/live/vmlinuz"
+cp "$BUILD_DIR"/boot/initrd.img-* "$ISO_DIR/live/initrd.img"
+
+# 5. GRUB
+mkdir -p "$ISO_DIR/boot/grub"
+cat > "$ISO_DIR/boot/grub/grub.cfg" <<EOF
 set timeout=5
 set default=0
 
 menuentry "MyOS Live (LXDE)" {
-    linux /live/vmlinuz boot=live quiet splash
+    linux /live/vmlinuz boot=live quiet
     initrd /live/initrd.img
 }
 EOF
 
-# 6. 製作 ISO
-grub-mkrescue -o myos.iso $ISO_DIR
+# 6. ISO
+grub-mkrescue -o myos.iso "$ISO_DIR"
 
-echo "=== Build finished: myos.iso ==="
+echo "=== ✅ Build finished: myos.iso ==="
